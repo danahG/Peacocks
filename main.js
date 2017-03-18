96877aa20e16c832a1ea22c68140064f98fad592
danahG <danielageorgieva3107@gmail.com>
2017-03-18 16:39:04 +0200

    long posts page is ready

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// attach an event listener to add class to html element
+
 window.onload = function () {
     let button = document.getElementById('welcome');
     button.addEventListener('click', function() {
@@ -5,3 +7,36 @@ window.onload = function () {
           elements.classList.add('feathers-active');
     })
 };
+
+// read more/less functionality
+
+function toggleSeeMoreOne() {
+    if(document.getElementById("textarea-one").style.display == 'none') {
+        document.getElementById("textarea-one").style.display = 'inline-block';
+        document.getElementById("seeMore-one").innerHTML = '<i class="fa fa-angle-double-up" aria-hidden="true"></i>';
+    } else {
+        document.getElementById("textarea-one").style.display = 'none';
+        document.getElementById("seeMore-one").innerHTML = '<i class="fa fa-angle-double-down" aria-hidden="true"></i>';
+    }
+}
+
+function toggleSeeMoreTwo() {
+    if(document.getElementById("textarea-two").style.display == 'none') {
+        document.getElementById("textarea-two").style.display = 'inline-block';
+        document.getElementById("seeMore-two").innerHTML = '<i class="fa fa-angle-double-up" aria-hidden="true"></i>';
+    } else {
+        document.getElementById("textarea-two").style.display = 'none';
+        document.getElementById("seeMore-two").innerHTML = '<i class="fa fa-angle-double-down" aria-hidden="true"></i>';
+    }
+}
+
+function toggleSeeMoreThree() {
+    if(document.getElementById("textarea-three").style.display == 'none') {
+        document.getElementById("textarea-three").style.display = 'inline-block';
+        document.getElementById("seeMore-three").innerHTML = '<i class="fa fa-angle-double-up" aria-hidden="true"></i>';
+    } else {
+        document.getElementById("textarea-three").style.display = 'none';
+        document.getElementById("seeMore-three").innerHTML = '<i class="fa fa-angle-double-down" aria-hidden="true"></i>';
+    }
+}
+
